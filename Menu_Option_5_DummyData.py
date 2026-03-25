5ab5405a833e098a9763decc9c52919b6569ab5c
ShirMaymon <shirmail128@gmail.com>
2026-03-25 22:56:49 +0200

    Add users set to Machine dictionary in dummy data

--- Menu_Option_5_DummyData.py
+++ Menu_Option_5_DummyData.py
@@ -17,7 +17,7 @@ def Menu_Option5(Hosts,Users,Machines):
 
     Hosts = [{'Name': 'Esxi', 'CPU': 200, 'Memory': 1000, 'Storage': 3000, 'Count': 5}, {'Name': 'Dell', 'CPU': 400, 'Memory': 1600, 'Storage': 5000, 'Count': 10}, {'Name': 'Lenovo', 'CPU': 500, 'Memory': 2500, 'Storage': 9000, 'Count': 4}]
     Users = [{'Name': 'OfficeUsers', 'Count': 1000}, {'Name': 'Developers', 'Count': 100}, {'Name': 'IT', 'Count': 35}]
-    Machines = {'Office': {'CPU': 4, 'Memory': 8, 'Storage': 80}, 'Dev': {'CPU': 32, 'Memory': 64, 'Storage': 300}, 'IT': {'CPU': 8, 'Memory': 16, 'Storage': 150}}
+    Machines = {'Office': {'CPU': 4, 'Memory': 8, 'Storage': 80, 'Users': {'office'}}, 'Dev': {'CPU': 32, 'Memory': 64, 'Storage': 300, 'Users': {'dev'}}, 'IT': {'CPU': 8, 'Memory': 16, 'Storage': 150, 'Users': {'IT', 'mangers'}}}
 
     print(f"""\n#### Physical Servers Info ####""")
     for i in Hosts :
@@ -40,13 +40,14 @@ def Menu_Option5(Hosts,Users,Machines):
         
     print("\n#### Virtual Machines Types Info ####")
     for i in Machines :
-        i2 = Machines[i]
-        print(f"""
+                i2 = Machines[i]
+                user_pop_list = i2.get('Users', "No populations assigned")
+                print(f"""
     Type name : {i}
     CPU for this type - {i2['CPU']}
     Memory for this type - {i2['Memory']}
     Storage for this type - {i2['Storage']}
-
+    Users Populations for this type - {user_pop_list}
     """)
 
     return(Hosts,Users,Machines)
